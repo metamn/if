eceb135f173ea01343481000fbc3a96e3d23fc99
metamn <bartus.csongor@gmail.com>
2014-02-09 11:34:42 +0200

    A first gist added with jQuery

--- assets/jquery.init.js
+++ assets/jquery.init.js
@@ -1,18 +1,27 @@
-var scrollSpeed = 70;
+function loadgist(gistid, filename, filetype) {
+
+$.ajax({
+  url: 'https://api.github.com/gists/'+gistid,
+  type: 'GET',
+  dataType: 'jsonp'
+}).success( function(gistdata) {
+    var content = gistdata.data.files[filename].content;
+    DoSomethingWith(content, filetype)
+  }).error( function(e) {
+  // ajax error
+});
     
-// set the default position
-var current = 0;
-
-// set the direction
-var direction = 'h';
-
-function bgscroll() {
-  // 1 pixel row at a time
-  current -= 1;
-
-  // move the background with backgrond-position css properties
-  $('#thumbs article, #desktop, #smartphone, #tablet').css("backgroundPosition", (direction === 'h') ? current + "px 0" : "0 " + current + "px");
 }
 
-// Calls the scrolling function repeatedly
-setInterval(bgscroll, scrollSpeed);    
+function DoSomethingWith(content, filetype) {
+  if (filetype == 'html') {
+    $('#article').append(content);
+  }
+  
+  if (filetype == 'css') {
+    $('#article').append('<style>' + content + '</style>');
+  }
+}
+
+loadgist(8896256, "index.html", 'html');
+loadgist(8896256, "style.css", 'css');
